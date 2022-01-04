79c7a0684c08c4636619abe130023419ef7a976f
Shayne Lemons <lemonsshayne6@gmail.com>
2022-01-04 15:17:57 -0800

    add functionality for shopping cart

--- client/src/utils/GlobalState.js
+++ client/src/utils/GlobalState.js
@@ -4,6 +4,8 @@ import { useProductReducer } from './reducers';
 const StoreProvider = ({ value = [], ...props }) => {
     const [state, dispatch] = useProductReducer({
         products: [],
+        cart: [],
+        cartOpen: false,
         categories: [],
         currentCategory: '',
     });
